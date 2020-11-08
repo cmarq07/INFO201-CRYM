77410d6ad490e13136fa444969b3f41139f1e38f
Rachel Wang <rachelziyiwang@gmail.com>
2020-11-07 18:15:12 -0800

    Change names of files

--- summary_calculations.R
+++ summary_calculations.R
@@ -3,6 +3,6 @@ library("readxl")
 
 total <- read_excel("INFO_total.xlsx")
 
-demographics <- read_excel("Count Us In (4).xlsx")
+demographics <- read_excel("INFO_demographics.xlsx")
 
-region <- read_excel("Count Us In (3).xlsx")
+region <- read_excel("INFO_region.xlsx")
