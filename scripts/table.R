397ceb01593e6ee3a33be6fbd27136fbdd57a61e
Rachel Wang <rachelziyiwang@gmail.com>
2020-11-12 17:33:36 -0800

    Create tables 1 and 2

--- scripts/table.R
+++ scripts/table.R
@@ -1,1 +1,6 @@
+source("scripts/summary_calculations.R")
 
+table_1 <- left_join(race_highest_percentage, region_highest_pop, 
+           by = c("Year"))
+
+table_2 <- left_join(table_1, total, by = c("Year"))
