f180a4abf001038f6be91354311ca51813ebf39a
Rachel Wang <rachelziyiwang@gmail.com>
2020-11-12 18:52:03 -0800

    Add table 3

--- scripts/table.R
+++ scripts/table.R
@@ -4,3 +4,8 @@ table_1 <- left_join(race_highest_percentage, region_highest_pop,
            by = c("Year"))
 
 table_2 <- left_join(table_1, total, by = c("Year"))
+
+table_3 <- left_join(table_2, highest_duration, by = c("Year")) %>% 
+  relocate("Year", "Homeless population", "King County population", 
+           "Percent of population")
+
