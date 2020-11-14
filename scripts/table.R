d8123e1ba3cb837e245551219e76e09fe9baf56e
Rachel Wang <rachelziyiwang@gmail.com>
2020-11-13 20:51:05 -0800

    Change table

--- scripts/table.R
+++ scripts/table.R
@@ -1,11 +1,7 @@
 source("scripts/summary_calculations.R")
 
-table_1 <- left_join(race_highest_percentage, region_highest_pop, 
+table_1 <- left_join(race_highest_percentage, region_highest_pop_rounded, 
            by = c("Year"))
 
-table_2 <- left_join(table_1, total, by = c("Year"))
-
-table_3 <- left_join(table_2, highest_duration, by = c("Year")) %>% 
-  relocate("Year", "Homeless population", "King County population", 
-           "Percent of population")
+table_2 <- left_join(table_1, highest_duration, by = c("Year"))
 
