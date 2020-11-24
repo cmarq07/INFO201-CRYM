fdcd3dd8d344fe3d198cf3797f26cae0f3945048
My-An Pham <phammy22@uw.edu>
2020-11-23 23:27:33 -0800

    summary calculations fixed

--- scripts/summary_calculations.R
+++ scripts/summary_calculations.R
@@ -2,19 +2,19 @@ library("httr")
 library("readxl")
 
 #data set for total number of homeless pop in Seattle
-total <- read_excel("INFO_total.xlsx")
+total <- read.csv("INFO_total.csv")
 
 #data set for demographics of homeless pop in Seattle
-demographics <- read_excel("INFO_demographics.xlsx")
+demographics <- read.csv("INFO_demographics.csv")
 
 #data set for different regions of homeless pop in Seattle
-region <- read_excel("INFO_region.xlsx")
+region <- read.csv("INFO_region.csv")
 
 #data set for duration of homeless pop in Seattle
-duration_of_homeless <- read_excel("INFO_duration_of_homeless.xlsx")
+duration_of_homeless <- read.csv("INFO_duration_of_homeless.csv")
 
 #data set for causes of homeless in Seattle
-cause <- read_excel("INFO_cause.xlsx")
+cause <- read.csv("INFO_cause.csv")
 
 #filtered data set to just race and ethnicity
 race_and_ethnicity <- demographics %>% 
@@ -29,55 +29,56 @@ race_highest_percentage <- race_and_ethnicity %>%
   rename(Top_race = Characteristic) %>% 
   rename(Race_percent = Percent)
 
-#find the race with the highest homeless pop and its percentage of the total 
-#homeless pop in Seattle
-highest_race_2020 <- race_highest_percentage %>% 
+#summary information list
+summary_info <- list()
+summary_info$highest_race_2020 <- race_highest_percentage %>% 
   filter(Year == "2020") %>% 
   pull(Top_race) 
-
-highest_race_2019 <- race_highest_percentage %>% 
+summary_info$highest_race_2019 <- race_highest_percentage %>% 
   filter(Year == "2019") %>% 
-  pull(Top_race) 
-
-highest_race_2018 <- race_highest_percentage %>% 
-  filter(Year == "2018") %>% 
-  pull(Top_race) 
-
-highest_race_2017 <- race_highest_percentage %>% 
-  filter(Year == "2017") %>% 
-  pull(Top_race) 
-
-highest_race_2016 <- race_highest_percentage %>% 
-  filter(Year == "2016") %>% 
-  pull(Top_race) 
-
-highest_race_2015 <- race_highest_percentage %>% 
-  filter(Year == "2015") %>% 
-  pull(Top_race) 
-
-highest_percentage_2020 <- race_highest_percentage %>% 
-  filter(Year == "2020") %>% 
   pull(Top_race)
-
-highest_percentage_2019 <- race_highest_percentage %>% 
+summary_info$highest_race_2018 <- race_highest_percentage %>% 
+  filter(Year == "2018") %>% 
+  pull(Top_race)
+summary_info$highest_race_2017 <- race_highest_percentage %>% 
+  filter(Year == "2017") %>% 
+  pull(Top_race)
+summary_info$highest_race_2016 <- race_highest_percentage %>% 
+  filter(Year == "2016") %>% 
+  pull(Top_race)
+summary_info$highest_race_2015 <- race_highest_percentage %>% 
+  filter(Year == "2015") %>% 
+  pull(Top_race)
+summary_info$highest_percentage_2020 <- race_highest_percentage %>% 
+  filter(Year == "2020") %>% 
+  pull(Race_percent)
+summary_info$highest_percentage_2019 <- race_highest_percentage %>% 
   filter(Year == "2019") %>% 
   pull(Race_percent)
-
-highest_percentage_2018 <- race_highest_percentage %>% 
+summary_info$highest_percentage_2018 <- race_highest_percentage %>% 
   filter(Year == "2018") %>% 
   pull(Race_percent)
-
-highest_percentage_2017 <- race_highest_percentage %>% 
+summary_info$highest_percentage_2017 <- race_highest_percentage %>% 
   filter(Year == "2017") %>% 
   pull(Race_percent)
-
-highest_percentage_2016 <- race_highest_percentage %>% 
+summary_info$highest_percentage_2016 <- race_highest_percentage %>% 
   filter(Year == "2016") %>% 
   pull(Race_percent)
-
-highest_percentage_2015 <- race_highest_percentage %>% 
+summary_info$highest_percentage_2015 <- race_highest_percentage %>% 
   filter(Year == "2015") %>% 
   pull(Race_percent)
+summary_info$top_region <- region_highest_pop_rounded %>% 
+  filter(Year == "2020") %>% 
+  pull(Top_region)
+summary_info$region_percent_2017 <- region_highest_pop_rounded %>% 
+  filter(Year == "2017") %>% 
+  pull(Region_percentage)
+summary_info$region_percent_2020 <- region_highest_pop_rounded %>% 
+  filter(Year == "2020") %>% 
+  pull(Region_percentage)
+summary_info$highest_duration_by_year <- highest_duration %>% 
+  filter(Year == "2020") %>% 
+  pull(Top_time_period)
 
 #region with the highest percentage of homeless for each year in Seattle
 region_highest_pop <- region %>% 
@@ -94,31 +95,11 @@ region_highest_pop <- region %>%
 #round the percentage
 region_highest_pop_rounded <- region_highest_pop %>% 
   mutate_at("Region_percentage", signif, 2)
-  
-top_region <- region_highest_pop_rounded %>% 
-  filter(Year == "2020") %>% 
-  pull(Top_region)
-
-region_percent_2017 <- region_highest_pop_rounded %>% 
-  filter(Year == "2017") %>% 
-  pull(Region_percentage)
-
-region_percent_2020 <- region_highest_pop_rounded %>% 
-  filter(Year == "2020") %>% 
-  pull(Region_percentage)
 
 #highest duration of homeless for each year in Seattle
 highest_duration <- duration_of_homeless %>% 
   group_by(Year) %>% 
   filter(Count == max(Count)) %>% 
-  rename(Top_time_period = `Time period`) %>% 
+  rename(Top_time_period = Time.period) %>% 
   rename(Time_period_count = Count) %>% 
-  rename(Time_period_percent = `Percent `)
-
-highest_duration_by_year <- highest_duration %>% 
-  filter(Year == "2020") %>% 
-  pull(Top_time_period)
-
-  
-
-
+  rename(Time_period_percent = Percent)
