40f9e3008ce711b6fbb4c0721e9d797197048587
Rachel Wang <rachelziyiwang@gmail.com>
2020-12-05 18:09:31 -0800

    Add initial

--- app_server.R
+++ app_server.R
@@ -1,1 +1,5 @@
-
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(stringr)
--- app_ui.R
+++ app_ui.R
@@ -1,1 +1,11 @@
+library(shiny)
+library(ggplot2)
+library(RColorBrewer)
+library(plotly)
+
+cause <- read.csv("INFO_cause.csv")
+demographics <- read.csv("INFO_demographics.csv")
+duration <- read.csv("INFO_duration_of_homeless.csv")
+region <- read.csv("INFO_region.csv")
+total <- read.csv("INFO_total.csv")
 
