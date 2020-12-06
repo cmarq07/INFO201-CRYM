7205e2193934ec3aa8e189cee6ff4422ef5dbd7c
Rachel Wang <rachelziyiwang@gmail.com>
2020-12-05 18:18:15 -0800

    Set up panels

--- app_ui.R
+++ app_ui.R
@@ -9,6 +9,31 @@ duration <- read.csv("INFO_duration_of_homeless.csv")
 region <- read.csv("INFO_region.csv")
 total <- read.csv("INFO_total.csv")
 
+intro_panel <- tabPanel(
+  "Introduction"
+)
+
+chart1_panel <- tabPanel(
+  "1st Chart"
+)
+
+chart2_panel <- tabPanel(
+  "2nd Chart"
+)
+
+chart3_panel <- tabPanel(
+  "3rd Chart"
+)
+
+conclusion_panel <- tabPanel(
+  "Conclusion"
+)
+
 ui <- navbarPage(
-  
+  "Final Deliverable: Homelessness in Seattle",
+  intro_panel,
+  chart1_panel,
+  chart2_panel,
+  chart3_panel,
+  conclusion_panel
 )
