3bcc5fb18632b63a1cd3434ad0208303244361ba
Rachel Wang <rachelziyiwang@gmail.com>
2020-12-05 21:49:09 -0800

    Add third chart data (Rachel)

--- app_server.R
+++ app_server.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(plotly)
 library(stringr)
 
+#load datasets
 cause <- read.csv("INFO_cause.csv")
 demographics <- read.csv("INFO_demographics.csv")
 duration <- read.csv("INFO_duration_of_homeless.csv")
@@ -11,5 +12,21 @@ region <- read.csv("INFO_region.csv")
 total <- read.csv("INFO_total.csv")
 
 server <- function(input, output) {
-  
+  output$chart3 <- renderPlotly({
+    
+    cause_updated <- cause %>% 
+      filter(Year == input$year_input) %>% 
+      select(Cause, Count)
+    
+    plot <- ggplot(data = cause_updated) +
+      geom_col(mapping = aes_string(x = "Cause", y = "Count"),
+               color = input$color_input) +
+      labs(x = "Causes", y = "Number of Instances", 
+           title = paste0("Count of Causes of Homelessness in Seattle in ", 
+                     input$year_input)) +
+      coord_flip()
+    
+    ggplotly(plot)
+    
+  })
 }
--- app_ui.R
+++ app_ui.R
@@ -3,11 +3,15 @@ library(ggplot2)
 library(RColorBrewer)
 library(plotly)
 
+#load datasets
 cause <- read.csv("INFO_cause.csv")
 demographics <- read.csv("INFO_demographics.csv")
 duration <- read.csv("INFO_duration_of_homeless.csv")
 region <- read.csv("INFO_region.csv")
 total <- read.csv("INFO_total.csv")
+
+cause_year_choices <- c(unique(cause$Year))
+cause_choices <- c(unique(cause$Cause))
 
 intro_panel <- tabPanel(
   "Introduction"
@@ -22,7 +26,26 @@ chart2_panel <- tabPanel(
 )
 
 chart3_panel <- tabPanel(
-  "3rd Chart"
+  "3rd Chart",
+  titlePanel("Bar Chart of Causes of Homelessness in Seattle"),
+  sidebarLayout(
+    sidebarPanel(
+      p("Choose a year and a color to display your bar chart!"),
+      year_input <- selectInput(
+        inputId = "year_input",
+        label = h3("Choose a year"),
+        choices = cause_year_choices
+      ),
+      color_input <- selectInput(
+        inputId = "color_input",
+        label = h3("Choose a color"),
+        choices = brewer.pal(10, "Set3")
+      )
+    ),
+    mainPanel(
+      plotlyOutput("chart3")
+    )
+  )
 )
 
 conclusion_panel <- tabPanel(
